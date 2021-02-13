bcb1513cdaa6bb464f660692ac87e99882d88939
unknown <jhanvisdesai@gmail.com>
2021-02-13 14:40:04 +0530

    Login page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FormComponent from "./components/FormComponent";
+// import hospbg1 from "./assets/hospbg1.jpg";
+
+function App() {
+  return (
+    <>
+      <FormComponent />
+    </>
+  );
+}
+
+export default App;
